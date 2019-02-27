2b5ae5a30c3f873e63315392838a9d88ec89c15c
Artem Kaloev <kaloev.eltech@gmail.com>
2019-02-27 18:45:49 +0300

    updated inputs layout

---
+++ src/scripts/modules/chart.js
@@ -0,0 +1,135 @@
+export default function chart() {
+
+  const svg = document.querySelector('.chart__svg');
+if (svg === null) return;
+  // The smoothing ratio
+const smoothing = 0.2
+
+const points = [
+  [0, 48],
+  [25, 45],
+  [50, 43],
+  [75, 35],
+  [100, 25],
+  [150, 20],
+  [175, 13],
+  [200, 3],
+]
+
+// Properties of a line 
+// I:  - pointA (array) [x,y]: coordinates
+//     - pointB (array) [x,y]: coordinates
+// O:  - (object) { length: l, angle: a }: properties of the line
+const line = (pointA, pointB) => {
+  const lengthX = pointB[0] - pointA[0]
+  const lengthY = pointB[1] - pointA[1]
+  return {
+    length: Math.sqrt(Math.pow(lengthX, 2) + Math.pow(lengthY, 2)),
+    angle: Math.atan2(lengthY, lengthX)
+  }
+}
+
+// Position of a control point 
+// I:  - current (array) [x, y]: current point coordinates
+//     - previous (array) [x, y]: previous point coordinates
+//     - next (array) [x, y]: next point coordinates
+//     - reverse (boolean, optional): sets the direction
+// O:  - (array) [x,y]: a tuple of coordinates
+const controlPoint = (current, previous, next, reverse) => {
+
+  // When 'current' is the first or last point of the array
+  // 'previous' or 'next' don't exist.
+  // Replace with 'current'
+  const p = previous || current
+  const n = next || current
+
+  // Properties of the opposed-line
+  const o = line(p, n)
+
+  // If is end-control-point, add PI to the angle to go backward
+  const angle = o.angle + (reverse ? Math.PI : 0)
+  const length = o.length * smoothing
+
+  // The control point position is relative to the current point
+  const x = current[0] + Math.cos(angle) * length
+  const y = current[1] + Math.sin(angle) * length
+  return [x, y]
+}
+
+// Create the bezier curve command 
+// I:  - point (array) [x,y]: current point coordinates
+//     - i (integer): index of 'point' in the array 'a'
+//     - a (array): complete array of points coordinates
+// O:  - (string) 'C x2,y2 x1,y1 x,y': SVG cubic bezier C command
+const bezierCommand = (point, i, a) => {
+
+  // start control point
+  const cps = controlPoint(a[i - 1], a[i - 2], point)
+
+  // end control point
+  const cpe = controlPoint(point, a[i - 1], a[i + 1], true)
+  return `C ${cps[0]},${cps[1]} ${cpe[0]},${cpe[1]} ${point[0]},${point[1]}`
+}
+
+// Render the svg <path> element 
+// I:  - points (array): points coordinates
+//     - command (function)
+//       I:  - point (array) [x,y]: current point coordinates
+//           - i (integer): index of 'point' in the array 'a'
+//           - a (array): complete array of points coordinates
+//       O:  - (string) a svg path command
+// O:  - (string): a Svg <path> element
+const svgPath = (points, command) => {
+  // build the d attributes by looping over the points
+  const d = points.reduce((acc, point, i, a) => i === 0
+    ? `M ${point[0]},${point[1]}`
+    : `${acc} ${command(point, i, a)}`
+  , '')
+  return `<defs>
+            <linearGradient id="g1" gradientUnits="userSpaceOnUse" x1="82.78%" y1="-9.49%" x2="17.22%" y2="109.49%">
+            <stop offset=".13" stop-color="#6F6EFF"/>
+            <stop offset=".93" stop-color="#2D29A7"/>
+            </linearGradient>
+            <g id="pn">
+              <text class="chart__text">Пн</text>
+              <text class="chart__text">10.03</text>
+            </g>
+          </defs>
+          <path d="${d} L200,50 L0,50 z" fill="url(#g1)" stroke="none" />
+          <rect x="0" y="50" width="200" height="50" fill="url(#g1)"></rect>
+          <line x1="0" x2="0" y1="50" y2="0" stroke="none"></line>
+          <g transform="translate(15,75)">
+              <text transform="translate(3,0)" class="chart__text">Пн</text>
+              <text y="6" class="chart__text">10.03</text>
+            </g>
+            <g transform="translate(40,75)">
+              <text transform="translate(3,0)" class="chart__text">Вт</text>
+              <text y="6" class="chart__text">11.03</text>
+            </g>
+            <g transform="translate(65,75)">
+              <text transform="translate(3,0)" class="chart__text">Ср</text>
+              <text y="6" class="chart__text">12.03</text>
+            </g>
+            <g transform="translate(90,75)">
+              <text transform="translate(3,0)" class="chart__text">Чт</text>
+              <text y="6" class="chart__text">13.03</text>
+            </g>
+            <g transform="translate(115,75)">
+              <text transform="translate(3,0)" class="chart__text">Пт</text>
+              <text y="6" class="chart__text">14.03</text>
+            </g>
+            <g transform="translate(140,75)">
+              <text transform="translate(3,0)" class="chart__text">Сб</text>
+              <text y="6" class="chart__text">15.03</text>
+            </g>
+            <g transform="translate(165,75)">
+              <text transform="translate(3,0)" class="chart__text">Вс</text>
+              <text y="6" class="chart__text">16.03</text>
+            </g>
+          `;
+}
+
+
+svg.innerHTML = svgPath(points, bezierCommand);
+
+}
---
+++ src/scripts/modules/inputs.js
@@ -0,0 +1,174 @@
+import { first } from "rxjs/operator/first";
+
+export default function inputs() {
+  const infoTypes = document.querySelector('.web-create__info-types');
+  if (infoTypes === null) return;
+
+  function createInput(label, className) {
+    let inputBlock = document.createElement('li');
+    inputBlock.classList.add('inputs__item');
+    inputBlock.classList.add(className);
+    inputBlock.innerHTML = `<label for="" class="inputs__label">
+      <div class="inputs__text">${label}</div>
+      <input type="text" class="inputs__input">
+      <div class="inputs__hint"></div>
+    </label>`;
+    return inputBlock;
+  }
+
+  function createPanel(panelClass, inputLabels) {
+    let inputs = document.createElement('ul');
+    // console.log(inputLabels)
+    inputLabels.forEach(el => inputs.appendChild(createInput(el)));
+    // inputs.appendChild(createInput('Contact1'));
+    // inputs.appendChild(createInput('Contact2'));
+    // inputs.appendChild(createInput('Contact3'));
+
+
+    let panel = document.createElement('div');
+    panel.classList.add('panel');
+    panel.classList.add('panel--web-create');
+    panel.classList.add(`${panelClass}`);
+    panel.appendChild(inputs);
+    
+    return panel;
+  }
+
+  function removeBlocks(removeClass, parent) {
+    const removeBlocks = document.querySelectorAll(removeClass);
+    const parentBlock = document.querySelector(parent);
+    if (removeBlocks !== null && parentBlock !== null) {
+      removeBlocks.forEach(el => parentBlock.removeChild(el));
+    }
+  }
+
+  const wrapper = document.querySelector('.web-create__inputs-wrapper');
+  const next = document.querySelector('.panel--next');
+  const firstPanel = document.querySelector('.first-info');
+
+  function removeToIndivid(className) {
+    removeBlocks('.panel.add-IP', '.web-create__inputs-wrapper');
+    removeBlocks('li.add-IP', '.first-info');
+    removeBlocks('.panel.add-entity', '.web-create__inputs-wrapper');
+    removeBlocks('li.add-entity', '.first-info');
+
+    if (wrapper.querySelector('.default') === null) {
+      const defaultPanel = document.createElement('div');
+    defaultPanel.classList.add('panel');
+    defaultPanel.classList.add('default');
+    defaultPanel.innerHTML = `<ul class="inputs">
+    <li class="inputs__item">
+      <label for="" class="inputs__label">
+        <div class="inputs__text">Телефон</div>
+        <input type="text" class="inputs__input">
+        <div class="inputs__hint"></div>
+      </label>
+    </li>
+    <li class="inputs__item">
+      <label for="" class="inputs__label">
+        <div class="inputs__text">E-mail</div>
+        <input type="text" class="inputs__input tel">
+        <div class="inputs__hint"></div>
+      </label>
+    </li>
+    <li class="inputs__item">
+      <label for="" class="inputs__label">
+        <div class="inputs__text">Город, страна</div>
+        <input type="text" placeholder="например, Москва, Россия" class="inputs__input">
+        <div class="inputs__hint"></div>
+      </label>
+    </li>
+    <li class="inputs__item">
+      <label for="" class="inputs__label">
+        <div class="inputs__text">Дата рождения</div>
+        <input type="text" placeholder="дд.мм.гг" class="inputs__input">
+        <div class="inputs__hint"></div>
+      </label>
+    </li>
+    <li class="inputs__item">
+      <label for="" class="inputs__label">
+        <div class="inputs__text">Профессиональные интересы</div>
+        <input type="text" placeholder="например, психология, психоанализ" class="inputs__input">
+        <div class="inputs__hint"></div>
+      </label>
+    </li>        
+  </ul>`;
+      wrapper.insertBefore(defaultPanel, firstPanel.closest('.panel').nextSibling)
+    }
+    if (firstPanel.querySelector('.default') === null) {
+      firstPanel.appendChild(createInput('Фамилия', 'default'));
+      firstPanel.appendChild(createInput('Имя', 'default'));
+      firstPanel.appendChild(createInput('Отчество', 'default'));
+    }
+  }
+
+  function createEntityDefaultPanel() {
+    const panel = document.createElement('div');
+    panel.classList.add('panel');
+    panel.classList.add('add-entity');
+    panel.innerHTML = `<ul class="inputs">
+      <li class="inputs__item">
+        <label for="" class="inputs__label">
+          <div class="inputs__text">Юридический адрес</div>
+          <input type="text" placeholder="например, Москва, Россия" class="inputs__input inputs__input--addr">
+          <input type="text" placeholder="например, Невский пр., дом 1, кв. 1" class="inputs__input">
+        </label>
+      </li>
+      <li class="inputs__item">
+        <label for="" class="inputs__label">
+          <div class="inputs__text">Фактический адрес</div>
+          <label class="inputs__match">
+            <div class="inputs__match-text">Совпадает с юридическим</div>
+            <input type="checkbox" class="inputs__match-check">
+            <div class="inputs__match-fake"></div>
+          </label>
+          <input type="text" placeholder="например, Москва, Россия" class="inputs__input inputs__input--addr">
+          <input type="text" placeholder="например, Невский пр., дом 1, кв. 1" class="inputs__input">
+        </label>
+      </li>
+    </ul>`;
+  return panel;
+  }
+
+  function addIP() {
+    wrapper.insertBefore(createPanel('add-IP', ['Контактное лицо', 'E-mail','Телефон']), next);
+    firstPanel.appendChild(createInput('ИНН', 'add-IP'));
+    firstPanel.appendChild(createInput('ЕГРИП', 'add-IP'));
+  }
+
+  function addEntity() {
+    wrapper.insertBefore(createPanel('add-entity', ['Телефон компании', 'E-mail компании','Род деятельности']), next);
+    wrapper.insertBefore(createEntityDefaultPanel(), next);
+    wrapper.insertBefore(createPanel('add-entity', ['Контактное лицо', 'E-mail','Телефон']), next);
+    while (firstPanel.firstChild) {
+      firstPanel.removeChild(firstPanel.firstChild);
+    }
+    firstPanel.appendChild(createInput('Название компании', 'add-entity'));
+    firstPanel.appendChild(createInput('ИНН', 'add-entity'));
+    firstPanel.appendChild(createInput('КПП', 'add-entity'));
+    firstPanel.appendChild(createInput('ОГРН', 'add-entity'));
+    firstPanel.appendChild(createInput('ФИО Генерального директора', 'add-entity'));
+    firstPanel.appendChild(createInput('ФИО Генерального директора в родительном падеже', 'add-entity'));
+    firstPanel.appendChild(createInput('Действует на основании', 'add-entity'));
+    wrapper.removeChild(wrapper.querySelector('.default'));
+  }
+
+  const infoTypesList = Array.from(infoTypes.children);
+  infoTypesList.forEach(el => {
+    el.addEventListener('click', (e) => {
+      e.preventDefault();
+      const prevActiveItem = document.querySelector('.info-types__type--is-active');
+      if (el === prevActiveItem) return;
+      prevActiveItem.classList.remove('info-types__type--is-active');
+      removeToIndivid();
+
+      if (el.classList.contains('info-types__type--ip')) {
+        addIP();
+      } else if (el.classList.contains('info-types__type--entity')) {
+        addEntity();
+      }
+      el.classList.add('info-types__type--is-active');
+    });
+  });
+
+}
